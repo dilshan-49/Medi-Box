3f4584db4ce6c37952fe2bc0746b9e70bbb7fd9b
dilshan-49 <157507979+dilshan-49@users.noreply.github.com>
2025-02-16 23:41:29 +0530

    Set-up to work with Wokwi. No coding is done yet.

---
+++ src/displayfn/displayfn.cpp
@@ -0,0 +1,3 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_SSD1306.h> // OLED display library
---
+++ src/displayfn/displayfn.h
@@ -0,0 +1,4 @@
+#ifndef DISPLAYFN_H
+#define DISPLAYFN_H
+
+#endif
